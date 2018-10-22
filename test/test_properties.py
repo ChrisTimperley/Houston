ec0153aad75843464f4d49f9fe82cbeb07b95623
ChrisTimperley <christimperley@googlemail.com>
2018-10-22 12:52:47 -0400

    added fault-revealing test for System.name

--- test/test_properties.py
+++ test/test_properties.py
@@ -2,8 +2,13 @@ import pytest
 
 from houston.ardu.common import ArmDisarm
 from houston.ardu.copter.goto import GoTo as CopterGoTo
+from houston.ardu.copter import ArduCopter
 
 
 def test_command_uid():
     assert CopterGoTo.uid == 'ardu:copter:goto'
     assert ArmDisarm.uid == 'ardu:common:arm'
+
+
+def test_system_name():
+    assert ArduCopter.name == 'arducopter'
