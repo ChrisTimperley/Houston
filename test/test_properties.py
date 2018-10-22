102689eff2dd3b4651212200bb34e7bd9da3d7b2
ChrisTimperley <christimperley@googlemail.com>
2018-10-22 12:32:15 -0400

    added fault revealing test

--- test/test_properties.py
+++ test/test_properties.py
@@ -1,7 +1,9 @@
 import pytest
 
+from houston.ardu.common import ArmDisarm
 from houston.ardu.copter.goto import GoTo as CopterGoTo
 
 
 def test_command_uid():
     assert CopterGoTo.uid == 'ardu:copter:goto'
+    assert ArmDisarm.uid == 'houston.ardu.common.ArmDisarm'
