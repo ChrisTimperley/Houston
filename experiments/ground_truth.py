942689d738fa139b3138c2c0c75a14d411fe3e34
ChrisTimperley <christimperley@googlemail.com>
2019-01-15 21:29:21 -0500

    added build_mutant

--- experiments/ground_truth.py
+++ experiments/ground_truth.py
@@ -1,5 +1,6 @@
-from typing import Iterator, Tuple
+from typing import Iterator, Tuple, Set
 import argparse
+import functools
 import contextlib
 import logging
 import json
@@ -10,6 +11,8 @@ import bugzoo
 import boggart
 import rooibos
 import houston
+from houston.mission import Mission
+from houston.trace import CommandTrace, MissionTrace
 
 logger = logging.getLogger('houston')  # type: logging.Logger
 logger.setLevel(logging.DEBUG)
@@ -50,6 +53,35 @@ def launch_servers() -> Iterator[Tuple[bugzoo.Client, boggart.Client]]:
                 yield client_bugzoo, client_boggart
 
 
+
+def killing_mission(client_bugzoo: bugzoo.Client,
+                    client_boggart: boggart.Client,
+                    snapshot: bugzoo.Bug,
+                    mutation: boggart.Mutation
+                   ) -> Optional[Tuple[str, Mission, MissionTrace]]:
+
+    # build trace for each mission
+    pass
+
+
+def build_mutant(client_bugzoo: bugzoo.Client,
+                 client_boggart: boggart.Client,
+                 snapshot: bugzoo.Bug,
+                 mutation: boggart.Mutation
+                 ) -> Iterator[bugzoo.Container]:
+    """
+    Provisions a BugZoo container for a given mutation.
+    """
+    container = None  # type: Optional[bugzoo.Container]
+    mutant = client_boggart.mutate(snapshot, [mutation])
+    try:
+        container = client_bugzoo.containers.provision(mutant.snapshot)
+        yield container
+    finally:
+        del client_bugzoo.containers[container.id]
+    del client_boggart.mutants[mutant.uuid]
+
+
 def main():
     args = parse_args()
     setup_logging(verbose=args.verbose)
@@ -66,14 +98,14 @@ def main():
     # load the mutants
     try:
         with open(fn_mutants, 'r') as f:
-            mutants = [boggart.Mutation.from_dict(jsn['mutation'])
-                       for jsn in json.load(f)]
-            logger.debug("loaded %d mutants from database", len(mutants))
+            mutations = [boggart.Mutation.from_dict(jsn['mutation'])
+                         for jsn in json.load(f)]
+            logger.debug("loaded %d mutations from database", len(mutations))
     except Exception:
-        logger.exception("failed to load mutant database: %s", fn_mutants)
+        logger.exception("failed to load mutation database: %s", fn_mutants)
         sys.exit(1)
     except FileNotFoundError:
-        logger.error("mutant database file not found: %s", fn_mutants)
+        logger.error("mutation database file not found: %s", fn_mutants)
         sys.exit(1)
 
     # obtain a list of oracle traces
@@ -83,6 +115,12 @@ def main():
     # load the oracle dataset
     with launch_servers() as (client_bugzoo, client_boggart):
         snapshot = client_bugzoo.bugs[name_snapshot]
+        km = functools.partial(killing_mission, client_bugzoo, client_boggart, snapshot)
+        for mutation in mutations:
+            
+
+            res = killing_mission(mutant)
+            fn_oracle_trace, mission, trace_mutant = res
 
 
 if __name__ == '__main__':
